3f0290b4c9a05089eb65b53a562425c8884849f1
Joao Netto <jnetto@fyyb.com.br>
2020-05-15 02:07:30 -0300

    udemy/cod3r-graphql/schema-query/projeto - challenge product

--- udemy/cod3r-graphql/schema-query/projeto/index.js
+++ udemy/cod3r-graphql/schema-query/projeto/index.js
@@ -4,8 +4,9 @@ const typeDefs = gql`
     # Postos de entrada da API
     type Query {
         ola: String,
-        horaAtual: String
-        usuarioLogado: Usuario
+        horaAtual: String,
+        usuarioLogado: Usuario,
+        produtoEmDestaque: Produto
     }
 
     type Usuario {
@@ -16,12 +17,24 @@ const typeDefs = gql`
         salario: Float,
         vip: Boolean
     }
+
+    type Produto {
+        nome: String!,
+        preco: Float!,
+        desconto: Float,
+        precoComDesconto: Float
+    }
 `;
 
 const resolvers = {
     Usuario: {
         salario(usuario) {
             return usuario.salario_real
+        }
+    },
+    Produto: {
+        precoComDesconto(produto) {
+            return produto.preco * (1 - ((produto.desconto) ? produto.desconto : 0))
         }
     },
 
@@ -40,6 +53,13 @@ const resolvers = {
                 idade: (parseInt(new Date().getFullYear())  - 1992),
                 salario_real: 9999.99,
                 vip: true
+            }
+        },
+        produtoEmDestaque() {
+            return {
+                nome: 'Produto 1',
+                preco: 100.00,
+                desconto: 0.5
             }
         }
     }
