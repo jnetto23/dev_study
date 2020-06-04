56d849687caf43a5eae38dc8c2282d44b0bbc4b4
Joao Netto <jnetto@fyyb.com.br>
2020-06-04 18:47:42 -0300

    udemy/cod3r-graphql/schema-query/projeto - enum

--- udemy/cod3r-graphql/schema-query/projeto/data/db.js
+++ udemy/cod3r-graphql/schema-query/projeto/data/db.js
@@ -3,19 +3,22 @@ const usuarios = [{
     nome: 'Joao Netto',
     email: 'jnetto@fyyb.com.br',
     idade: 28,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'ATIVO'
 }, {
     id: 2,
     nome: 'Fernanda Marques',
     email: 'fernanda@email.com',
     idade: 31,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'INATIVO'
 }, {
     id: 3,
     nome: 'JJ',
     email: 'jj@email.com',
     idade: 1,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'BLOQUEADO'
 }];
 
 const perfis = [{
