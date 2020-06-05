a3009b731b394d465f2b2124068c67d12ec149cb
Joao Netto <jnetto@fyyb.com.br>
2020-06-04 21:28:53 -0300

    udemy/cod3r-graphql/mutation/projeto - example of parameter validation

--- udemy/cod3r-graphql/mutation/projeto/resolvers/Mutation.js
+++ udemy/cod3r-graphql/mutation/projeto/resolvers/Mutation.js
@@ -2,6 +2,12 @@ const { usuarios, proximoId } = require('../data/db');
 
 module.exports = {
     novoUsuario(_, { nome, email, idade }) {
+        const emailExistente = usuarios
+            .some(u => u.email === email);
+        if(emailExistente) {
+            throw new Error('Email cadastrado');
+        }
+        
         const novo = {
             id: proximoId(),
             nome,
